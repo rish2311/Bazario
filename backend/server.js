9a78bed37aa9bb791a596e7de8287a1739367c50
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-07-07 22:02:44 +0530

    created the cart and payment Routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 dotenv.config();
 
@@ -15,9 +17,11 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(5000, () => {
-  console.log("Server is running on port:", PORT);
+  console.log("Server is running on port:" + PORT);
 
   connectDB();
 });
