18e5fcac1a2222934f0e6d72b97c20c5a0f7269b
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-07-07 18:22:36 +0530

    backend setup, Implemented the auth, refresh and accessTokens

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { login, logout, signup, refreshToken } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
+
+router.get("/refresh-token", refreshToken);
+
+export default router;
+
